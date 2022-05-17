fca8a41165db3c0de0a2a640e8719b0fc3c495a2
mafgomes <mafgomes@senado.leg.br>
2022-05-17 06:15:06 -0300

    Melhoria nos comentários em pteste.h

--- pteste.h
+++ pteste.h
@@ -7,14 +7,15 @@
  *  Variáveis globais
  *-----------------------------------------------------------*/
 
-extern int Num_falhas;
-extern int Num_tarefas;
-extern int Num_casos;
+extern int Num_falhas;	// Casos em que esperado != obtido
+extern int Num_tarefas;	// Tarefas executando em paralelo
+extern int Num_casos;	// Total de casos a serem testados
 
 /*-----------------------------------------------------------*
  *  Funções globais
  *-----------------------------------------------------------*/
 
+// Função sob teste: recebe 2 ints; devolve 1 int
 extern int Combinacoes(int n, int r);
 
 #endif /* PTESTE_H */
